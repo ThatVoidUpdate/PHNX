11efd110f5af54c2e83c7f4f03f18a0eca8d2df0
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-05-20 17:48:17 +0100

    Moved foot script to more general bodypart script. Added walljump toggle. Removed air control

---
+++ Assets/Scripts/BodyCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BodyCollider : MonoBehaviour
+{
+    public UnityEvent TouchedEvent;
+    public UnityEvent ReleasedEvent;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TouchedEvent.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ReleasedEvent.Invoke();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,27 +9,47 @@ public class Player : MonoBehaviour
     public float HorizontalSpeed;
     public float JumpForce;
     public bool IsGrounded = true;
+    public bool IsTouchingLeftWall;
+    public bool IsTouchingRightWall;
+
+    public bool CanWallJump;
 
     private Rigidbody2D rb;
     private BoxCollider2D BodyCollider;
-    private BoxCollider2D FootCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         BodyCollider = GetComponent<BoxCollider2D>();
-        FootCollider = GetComponentInChildren<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float Horizontal = Input.GetAxis("Horizontal") * HorizontalSpeed;
+        float Horizontal = rb.velocity.x; ;
+
+        if (IsGrounded)
+        {
+            Horizontal = Input.GetAxis("Horizontal") * HorizontalSpeed;
+        }
+        
         float Vertical = 0;
         if (Input.GetAxis("Jump") == 1 && IsGrounded)
         {
             Vertical = JumpForce;
+        }
+        else if (Input.GetAxis("Jump") == 1 && IsTouchingLeftWall && CanWallJump)
+        {
+            //fire off at an up-right angle
+            Horizontal = JumpForce / 2;
+            Vertical = JumpForce / 2;
+        }
+        else if (Input.GetAxis("Jump") == 1 && IsTouchingRightWall && CanWallJump)
+        {
+            //fire off at an up-left angle
+            Horizontal = JumpForce / -2;
+            Vertical = JumpForce / 2;
         }
         else
         {
@@ -46,5 +66,25 @@ public class Player : MonoBehaviour
     public void BecomeUnGrounded()
     {
         IsGrounded = false;
+    }
+
+    public void LeftTouch()
+    {
+        IsTouchingLeftWall = true;
+    }
+
+    public void LeftRelease()
+    {
+        IsTouchingLeftWall = false;
+    }
+
+    public void RightTouch()
+    {
+        IsTouchingRightWall = true;
+    }
+
+    public void RightRelease()
+    {
+        IsTouchingRightWall = false;
     }
 }
